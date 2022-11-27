b6bb7649689d03f1023cd0b9805e1d70f710f934
Wadimabr <wadimabr@gmail.com>
2022-11-28 00:00:47 +0100

    fix

--- src/test/kotlin/TestAutomationEnvironment.kt
+++ src/test/kotlin/TestAutomationEnvironment.kt
@@ -16,23 +16,9 @@ class TestAutomationEnvironment {
 
     @Test
     fun runTest() {
-        val didYouKnowLink = "xpath"
-        val pageTitle = "xpath"
-
         WebTest(config).apply {
             driver(true) {
-                get("https://en.wikipedia.org/wiki/Main_Page")
-                waitUntilPageIsLoaded()
-                waitThenClick(didYouKnowLink)
-                waitMillis(100)
-                waitUntilPageIsLoaded()
-                waitUntilDisplayed(pageTitle)
-
-                val title = getText(pageTitle)
-                    .filter { it.isWhitespace() or it.isLetterOrDigit() }
-                    .replace(' ', '_')
-
-                takeScreenshot(title)
+                //write your code here
             }
         }
     }
